da1fadce3d531e3aa867155ce00d39ac5444aa3b
rick2785 <rjhrabowskie@gmail.com>
2025-02-15 19:31:46 -0600

    Initial Commit

---
+++ CustomAlerts/CustomAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertsApp.swift
+//  CustomAlerts
+//
+//  Created by RJ Hrabowskie on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
